bbec9ff9f0c60b5656e7d18bd3f9ef0a5c2f529d
serge <serge@outlook.com>
2023-08-25 14:34:50 +0100

    Navigation collapse and shadow

--- src/app/layout/navigation/navigation.component.ts
+++ src/app/layout/navigation/navigation.component.ts
@@ -1,27 +1,46 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import { AuthService } from "src/app/services/auth.service";
 
 @Component({
     selector: "app-navigation",
     templateUrl: "./navigation.component.html",
-    styleUrls: ["./navigation.component.scss"]
+    styleUrls: ["./navigation.component.scss"],
+    host: {
+        "(document:click)": "onDocumentClick($event)",
+    }
 })
-export class NavigationComponent {
+export class NavigationComponent implements OnInit {
 
     public isMenuActive = false;
 
-    constructor(private authService: AuthService, private router: Router) {
+    constructor(private authService: AuthService, private router: Router, private eref: ElementRef) {
     }
 
     get isAuthenticated(): boolean {
         return this.authService.isAuthenticated();
     }
-    
+
     get isAdmin(): boolean {
-        return this.authService.getRole() === "Admin";        
+        return this.authService.getRole() === "Admin";
     }
-    
+
+    ngOnInit(): void {
+        this.router.events.subscribe(() => {
+            if (this.isMenuActive) {
+                this.toggleMenu();
+            }
+        })
+    }
+
+    onDocumentClick(event: any) {
+        if (this.isMenuActive) {
+            if (!this.eref.nativeElement.contains(event.target)) {
+                this.toggleMenu();
+            }
+        }
+    }
+
     toggleMenu() {
         this.isMenuActive = !this.isMenuActive;
     }
@@ -30,6 +49,4 @@ export class NavigationComponent {
         this.authService.signOutExternal();
         this.router.navigate(["/"]);
     }
-
-    
 }
