314f447509308f856c0ebeb0cfdd060b77eac6a2
serge <serge@outlook.com>
2023-09-04 17:52:44 +0100

    Fixed build

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     apiDomain: "{{API_DOMAIN}}",
     apiUrl: "{{API_URL}}",
-    mapBoxToken: "{{MAPBOX_TOKEN}}"
+    mapBoxToken: "pk.eyJ1IjoiY29mZmVlaG9saWMiLCJhIjoiY2xtNHhobWkxMWxkbTNwczZkdHZxbHZ3diJ9.aV4fgkKenvH8LTDlR5462g"
 };
